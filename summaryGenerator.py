f756d6d59c0e6a6985ab6a7b243bbb4510788967
Singh <X6ASINGH@zeiss.com>
2019-02-19 10:05:40 +0530

    Saving the summary and failed topics from summary generator

--- summaryGenerator.py
+++ summaryGenerator.py
@@ -1,4 +1,5 @@
 import wikipedia
+import pickle
 from content import Alltopics
 summary = []
 failed = []
@@ -7,7 +8,10 @@ for topic in Alltopics():
         summary.append(wikipedia.summary(tuple((topic,str(topic)))))
     except Exception as e:
         failed.append(tuple((topic,e)))
+with open("summary.txt", "wb") as fp:
+    pickle.dump(summary , fp)
+with open('failed.txt', 'wb') as fp:
+    pickle.dump('failed', fp)
 
-print (summary[:5])
 
 
